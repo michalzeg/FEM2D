377f297281410d2a74685be00ed4f418d11dd063
Michal_2 <Michal_2@MICHAL-KOMPUTER>
2017-07-08 12:01:35 +0200

    added meshing algorithm and test application

---
+++ TestApplication/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using netDxf;
+using Triangulation;
+using netDxf.Entities;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var filePath = @"D:\test.dxf";
+
+            var document = new DxfDocument();
+
+            var meshing = new MeshCreator();
+            var triangles = meshing.Create();
+
+            foreach (var triangle in triangles)
+            {
+                var n1 = triangle.GetVertex(0);
+                var n2 = triangle.GetVertex(1);
+                var n3 = triangle.GetVertex(2);
+
+                var v1 = new Vector2(n1.X, n1.Y);
+                var v2 = new Vector2(n2.X, n2.Y);
+                var v3 = new Vector2(n3.X, n3.Y);
+
+                Face3d face = new Face3d(v1, v2, v3);
+                document.AddEntity(face);
+            }
+
+            document.Save(filePath);
+
+        }
+    }
+}
---
+++ Triangulation/MeshCreator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleNet.Geometry;
+using TriangleNet.Smoothing;
+using TriangleNet.Algorithm;
+using TriangleNet;
+using TriangleNet.Data;
+
+namespace Triangulation
+{
+    public class MeshCreator
+    {
+
+        public IEnumerable<Triangle> Create()
+        {
+            var numPoints = 6;
+
+            var geometry = new InputGeometry(numPoints);
+            geometry.AddPoint(0, 0);
+            //geometry.AddPoint(5, 0);
+
+            geometry.AddPoint(10, 0);
+            //geometry.AddPoint(10, 5);
+
+            geometry.AddPoint(10, 5);
+            //geometry.AddPoint(5, 10);
+            geometry.AddPoint(15, 5);
+            geometry.AddPoint(15, 10);
+            geometry.AddPoint(0, 10);
+            //geometry.AddPoint(0, 5);
+
+            for (int i = 0; i < numPoints-1; i++)
+            {
+                geometry.AddSegment(i, i + 1, 1);
+            }
+
+            geometry.AddSegment(numPoints - 1, 0);
+
+            geometry.AddSegment(0, 1,1);
+            geometry.AddSegment(1, 2,1);
+            geometry.AddSegment(2, 3,1);
+            geometry.AddSegment(3, 4,1);
+            geometry.AddSegment(4, 5, 1);
+            geometry.AddSegment(5, 6, 1);
+            geometry.AddSegment(6, 7, 1);
+            geometry.AddSegment(7, 0, 1);
+
+            
+            Mesh mesh = new Mesh();
+            mesh.Behavior.Quality = true;
+            mesh.Behavior.MaxArea = 0.1;
+            //mesh.Behavior.MaxAngle = 60;
+            //mesh.Behavior.MinAngle = 30;
+            mesh.Behavior.ConformingDelaunay = true;
+            mesh.Triangulate(geometry);
+            mesh.Smooth();
+            mesh.Refine();
+            return mesh.Triangles;
+ 
+        }
+    }
+}
