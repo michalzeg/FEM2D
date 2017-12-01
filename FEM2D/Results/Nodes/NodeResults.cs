79c37de16b535657206fe32898e815a73d4d8206
Michal_2 <Michal_2@MICHAL-KOMPUTER>
2017-12-01 17:06:36 +0100

    preparing for REsults refactoring

---
+++ FEM2D/Results/Nodes/NodeResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEM2D.Results.Nodes
+{
+    public class NodeResults
+    {
+        public NodeResults()
+        {
+
+        }
+    }
+}
