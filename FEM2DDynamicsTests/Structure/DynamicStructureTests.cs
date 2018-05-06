e2767e599604baca490a592ace01c545530cce8e
Michal_2 <Michal_2@MICHAL-KOMPUTER>
2018-05-06 13:47:16 +0200

    added test solution for Dynamics

---
+++ FEM2DDynamicsTests/Structure/DynamicStructureTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using FEM2DDynamics.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FEM2DDynamics.Structure.Tests
+{
+    [TestFixture()]
+    public class DynamicStructureTests
+    {
+        [Test()]
+        public void DynamicStructureTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void SolveTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
