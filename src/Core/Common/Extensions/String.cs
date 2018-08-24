820d8c58a04c37e47fbbec96d2f9cb44045fd154
Michal_2 <Michal_2@MICHAL-KOMPUTER>
2018-08-24 22:21:19 +0200

    fixed common

---
+++ src/Core/Common/Extensions/String.cs
@@ -0,0 +1,15 @@
+﻿namespace Common.Extensions
+{
+    public static class StringExtensionMethods
+    {
+        public static string ToFormatedString(this double initialValue)
+        {
+            return initialValue.Round().ToString("F");
+        }
+
+        public static string ToDot(this string value)
+        {
+            return value.Replace(',', '.');
+        }
+    }
+}
